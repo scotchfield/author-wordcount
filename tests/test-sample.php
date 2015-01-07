f7a01eb948963047eee4752d03a846ba0158dde3
scotchfield <swrittenb@gmail.com>
2015-01-06 22:30:24 -0500

    Add unit testing for plugin (basic, more coming)

---
+++ tests/test-sample.php
@@ -0,0 +1,23 @@
+<?php
+
+class Test_AuthorWordcount extends WP_UnitTestCase {
+
+	public function tearDown() {
+		delete_option( 'author_wordcount' );
+	}
+
+	/**
+	 * @covers WP_Author_Wordcount::__construct
+	 */
+	public function test_new() {
+		$class = new WP_Author_Wordcount();
+
+		$this->assertNotNull( $class );
+		$this->assertEquals( array(), $class->word_obj );
+	}
+
+
+
+
+}
+
